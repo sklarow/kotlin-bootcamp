17d234546389c35a634be8d3dba1c69a63606fac
Allan Sklarow <allan@sklarow.blog.br>
2020-09-22 14:29:36 -0300

    Adding 3 more exercises

---
+++ src/03_FortuneCookie.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookieOld()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+}
+
+fun getFortuneCookieOld(): String{
+    var fortunes = listOf("You will have a great day!",
+    "Things will go well for you today.",
+    "Enjoy a wonderful day of success.",
+    "Be humble and all will turn out well.",
+    "Today is a good day for exercising restraint.",
+    "Take it easy and enjoy life!",
+    "Treasure your friends because they are your greatest fortune.")
+
+    println("\nEnter your birthday: ")
+    val birthday: Int = readLine()?.toIntOrNull() ?: 1
+
+    return "${fortunes[birthday.rem(fortunes.size)]}"
+}
